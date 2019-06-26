60b2ae14c8b6ae957f183c4347a7aa2a240b76a9
Elena Piaggio <helenamercury@gmail.com>
2019-06-27 00:49:39 +0200

    add fourth iteration

--- js/index.js
+++ js/index.js
@@ -1,81 +1,85 @@
-function deleteItem(event){
-
+function deleteItem(event) {
+  let buttonToDelete = event.target.parentNode.parentNode;
+  let contentDiv = document.getElementsByClassName('wrapper-product');
+  for (let i = 0; i < contentDiv.length; i++) {
+    if (contentDiv[i] === buttonToDelete) {
+      contentDiv[i].remove(buttonToDelete);
+    }
+  }
 }
 
-function getPriceByProduct(itemNode, position){ 
+function getPriceByProduct(itemNode, position) {
   let elements = document.getElementsByClassName(itemNode);
-  for(let i = 0; i < elements.length; i++ ){
-    if(i === position){
+  for (let i = 0; i < elements.length; i++) {
+    if (i === position) {
       return elements[i].innerHTML;
     }
   }
 }
 
-function getQuantityOfProducts(itemNode, position){
+function getQuantityOfProducts(itemNode, position) {
   return document.getElementsByName(itemNode)[position].value;
 }
 
-function updatePriceByProduct(productPrice, index){
+function updatePriceByProduct(productPrice, index) {
   let nodeTotalPrice = document.getElementsByClassName(productPrice);
   let totalPriceProducts = 0;
-  for(let i = 0; i < index.length; i++){
+  for (let i = 0; i < index.length; i++) {
     totalPriceProducts += getTotalPrice(i);
-      nodeTotalPrice[i].innerHTML = getTotalPrice(i);
-  } 
+    nodeTotalPrice[i].innerHTML = getTotalPrice(i);
+  }
 
   let totalShoppingCart = document.getElementsByClassName('total');
-  console.log(`total carrito: ${totalPriceProducts}`);
-
-  for(let i = 0; i < totalShoppingCart.length; i++){
+  for (let i = 0; i < totalShoppingCart.length; i++) {
     totalShoppingCart[i].innerHTML = totalPriceProducts;
   }
 
   let displayNone = document.getElementById('total-shopping-cart');
   displayNone.removeAttribute('id');
   displayNone.setAttribute('class', 'total-shopping-cart-true');
+
 }
 
 function getTotalPrice(position) {
   return this.getPriceByProduct('cost-unit', position) * this.getQuantityOfProducts('quantity', position);
-  
-}
-
-function createQuantityInput(){
-  
-}
-
-function createDeleteButton(){
 
 }
 
-function createQuantityNode(){
+function createQuantityInput() {
 
 }
 
-function createItemNode(dataType, itemData){
+function createDeleteButton() {
 
 }
 
-function createNewItemRow(itemName, itemUnitPrice){
+function createQuantityNode() {
 
 }
 
-function createNewItem(){
+function createItemNode(dataType, itemData) {
 
 }
 
-window.onload = function(){
+function createNewItemRow(itemName, itemUnitPrice) {
+
+}
+
+function createNewItem() {
+
+}
+
+window.onload = function () {
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
 
   let btnCalcPricesButton = document.querySelector("#calc-prices-button");
-  let totalProducts = document.getElementsByClassName('wrapper-product');
   btnCalcPricesButton.setAttribute("onclick", "updatePriceByProduct('total-price', document.getElementsByClassName('wrapper-product'))");
 
 
+  var deleteButtons = document.getElementsByClassName('btn-delete');
 
-  for(var i = 0; i<deleteButtons.length ; i++){
+  for (var i = 0; i < deleteButtons.length; i++) {
     deleteButtons[i].onclick = deleteItem;
   }
 };
